aff0cdfa4f54c773060845d9d81480d33d4a1c19
Sanyog Singh Rajput <sanyogsingrajput@gmail.com>
2023-05-06 21:26:40 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        backgroundColor: Colors.blue,
+        
+      ),
+
+      appBar: AppBar(
+        title: Text('Ui'),
+      ),
+    );
+  }
+}
